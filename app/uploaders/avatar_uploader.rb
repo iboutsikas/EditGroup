ffd1f483d5de89d392934fe18ee922a316b8a6e3
harry bournis <harrybournis@gmail.com>
2016-06-08 16:52:05 +0300

    fixed images

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,8 +3,8 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  #include CarrierWave::RMagick
+  #include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -33,9 +33,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-   process resize_to_fit: [70, 70]
-  end
+  # version :thumb do
+  #  process resize_to_fit: [70, 70]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
