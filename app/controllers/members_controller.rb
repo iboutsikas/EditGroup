32668c78d1fffab9b9abd842b0d463009a637302
harry bournis <harrybournis@gmail.com>
2016-06-15 16:00:41 +0300

    somtehtid

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,32 +1,10 @@
-require 'pry'
-
 class MembersController < ApplicationController
-  before_action :authenticate_member!, except: [:index,:show]
-  before_action :set_member, only: [:show, :edit, :update, :destroy, :edit_profile]
-  before_filter :allow_if_current_member, only: [:edit_profile]
+  before_action :authenticate_member!, except: [:index]
+  before_action :set_member, only: [:update, :edit_profile]
+  before_filter :allow_if_current_member, only: [:edit_profile, :update]
 
-  # GET /members
-  # GET /members.json
   def index
     @members = Member.includes(:personal_websites,:participant,:person)
-  end
-
-  # GET /members/1
-  # GET /members/1.json
-  def show
-    @websites = @member.personal_websites
-  end
-
-  # GET /members/new
-  def new
-    @member = Member.new
-    @participant = @member.build_participant
-    @person = @member.build_person
-    @personal_website = resource.personal_websites.build
-  end
-
-  # GET /members/1/edit
-  def edit
   end
 
   def edit_profile
@@ -36,24 +14,6 @@ class MembersController < ApplicationController
     respond_to do |format|
       @edit = true
       format.html
-    end
-  end
-
-  # POST /members
-  # POST /members.json
-  def create
-    @member = Member.new(member_params.except(:person_attributes, :participant_attributes))
-    @member.participant = Participant.new(member_params[:participant_attributes])
-    @member.person = Person.new(member_params[:person_attributes])
-
-    respond_to do |format|
-      if @member.save
-        @member.participant.person_id = @member.person_id
-
-        format.js { render js: 'hide_and_redraw()' }
-      else
-        format.js { render 'admin/initializeForm', locals: {resource: @member, form_path: "members/form" } }
-      end
     end
   end
 
@@ -69,16 +29,6 @@ class MembersController < ApplicationController
         format.html { render :edit }
         format.json { render json: @member.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /members/1
-  # DELETE /members/1.json
-  def destroy
-    @member.destroy
-    respond_to do |format|
-      format.html { redirect_to members_url, notice: 'Member was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
