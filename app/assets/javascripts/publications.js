bdb04189dbe171cc9a2a8f9a39ddbaa5e2340f57
Ioannis Boutsikas <mgioanni@outlook.com>
2016-06-15 12:07:32 +0300

    Added two versions of publications

--- app/assets/javascripts/publications.js
+++ app/assets/javascripts/publications.js
@@ -1,5 +1,5 @@
 $(function() {
-  let timelineBlocks = $('.cd-timeline-block'),
+  let timelineBlocks = $('.publication-timeline-block'),
       offset = 0.8;
 
 	hideBlocks(timelineBlocks, offset);
@@ -13,14 +13,14 @@ $(function() {
 	function hideBlocks(blocks, offset) {
 		blocks.each(function(){
       let block = $(this);
-			(block.offset().top > $(window).scrollTop()+$(window).height()*offset ) && block.find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+			(block.offset().top > $(window).scrollTop()+$(window).height()*offset ) && block.find('.publication-timeline-img, .publication-timeline-content').addClass('is-hidden');
 		});
 	}
 
 	function showBlocks(blocks, offset) {
 		blocks.each(function(){
       let block = $(this);
-			(block.offset().top <= $(window).scrollTop()+$(window).height()*offset && block.find('.cd-timeline-img').hasClass('is-hidden')) && block.find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+			(block.offset().top <= $(window).scrollTop()+$(window).height()*offset && block.find('.publication-timeline-img').hasClass('is-hidden')) && block.find('.publication-timeline-img, .publication-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 		});
 	}
 
