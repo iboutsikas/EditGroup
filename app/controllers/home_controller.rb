a0ff93e6663ecfa8c3bd1cda5438848a3910f5b7
Ioannis Boutsikas <mgioanni@outlook.com>
2016-06-16 16:31:37 +0300

    added search button to publications

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -29,7 +29,7 @@ class HomeController < ApplicationController
   end
 
   def newsevents
-    @news = NewsEvent.all.group_by{|year| year.date.year desc}
+    @news = NewsEvent.all.order(date: :desc).group_by{|year| year.date.year}
   end
 
   def contact
