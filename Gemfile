d8050ea07b4d21f18c9a179490f273fca1431f84
Ioannis Boutsikas <mgioanni@outlook.com>
2016-06-20 17:58:24 +0300

    prepare for heroku

--- Gemfile
+++ Gemfile
@@ -3,8 +3,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -21,7 +20,6 @@ gem 'cocoon', '~> 1.2', '>= 1.2.8'
 
 gem 'sprockets-rails', '~> 3.0', '>= 3.0.4'
 
-#gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem 'bootstrap-sass', '3.3.6'
 
 gem 'devise', '~> 3.5', '>= 3.5.6'
@@ -84,9 +82,16 @@ gem 'ahoy_matey', '~> 1.4'
 # text editor
 gem 'tinymce-rails', '~> 4.3', '>= 4.3.13'
 
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
+end
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  # Use sqlite3 as the database for Active Record
+  gem 'sqlite3'
 end
 
 group :development do
--- app/assets/javascripts/publications.js
+++ app/assets/javascripts/publications.js
@@ -1,5 +1,5 @@
 $(function() {
-  let timelineBlocks = $('.publication-timeline-block'),
+  var timelineBlocks = $('.publication-timeline-block'),
       offset = 0.8;
 
 	hideBlocks(timelineBlocks, offset);
@@ -12,14 +12,14 @@ $(function() {
 
 	function hideBlocks(blocks, offset) {
 		blocks.each(function(){
-      let block = $(this);
+      var block = $(this);
 			(block.offset().top > $(window).scrollTop()+$(window).height()*offset ) && block.find('.publication-timeline-img, .publication-timeline-content').addClass('is-hidden');
 		});
 	}
 
 	function showBlocks(blocks, offset) {
 		blocks.each(function(){
-      let block = $(this);
+      var block = $(this);
 			(block.offset().top <= $(window).scrollTop()+$(window).height()*offset && block.find('.publication-timeline-img').hasClass('is-hidden')) && block.find('.publication-timeline-img, .publication-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 		});
 	}
